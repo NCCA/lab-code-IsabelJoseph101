81ad7cd47dcc9d90405e456a2cb3e5050697b2b0
isabel <s5221926@bournemouth.ac.uk>
2023-02-16 15:46:32 +0000

    Image test run

---
+++ Image/src/main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include <cstdlib>
+
+int main()
+{
+    std::cout<<"Image Demo\n";
+    return EXIT_SUCCESS;
+}
---
+++ Image/test/ImageTests.cpp
@@ -0,0 +1,7 @@
+#include <gtest/gtest.h>
+#include "Image.h"
+
+TEST(Image, ctor)
+{
+    auto img = Image();
+}
